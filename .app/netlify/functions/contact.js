eb26f45c359c385db9908b92d636e9c2fca0312b
Timo <timo@skillhub.ee>
2023-11-02 11:24:45 +0100

    push 4

--- .app/netlify/functions/contact.js
+++ .app/netlify/functions/contact.js
@@ -1,13 +1,13 @@
 import nodemailer from 'nodemailer'
 
-const config = useRuntimeConfig()
+const endpointConfig = useRuntimeConfig()
 
 const transporter = nodemailer.createTransport({
-  host: config.MAILHOST,
-  port: config.MAILPORT,
+  host: endpointConfig.MAILHOST,
+  port: endpointConfig.MAILPORT,
   auth: {
-    user: config.MAILUSER,
-    pass: config.MAILPASSWORD,
+    user: endpointConfig.MAILUSER,
+    pass: endpointConfig.MAILPASSWORD,
   },
 })
 
@@ -16,61 +16,15 @@ export default defineEventHandler(async (event, response) => {
     const body = await readBody(event)
 
     const mail = await transporter.sendMail({
-      from: config.CONTACTMAIL,
-      to: config.CONTACTMAIL,
+      from: endpointConfig.CONTACTMAIL,
+      to: endpointConfig.CONTACTMAIL,
       subject: body.subject,
       text: body.message,
       html: `
-              <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Form Submission</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #f4f4f4;
-        }
-        .email-container {
-            margin: auto;
-            padding: 20px;
-            width: 80%;
-            background-color: #fff;
-            border-radius: 8px;
-        }
-        .header {
-            font-size: 24px;
-            margin-bottom: 20px;
-            color: #333;
-        }
-        .content {
-            margin-bottom: 20px;
-            color: #555;
-        }
-        .footer {
-            font-size: 12px;
-            color: #888;
-        }
-    </style>
-</head>
-<body>
-    <div class="email-container">
-        <div class="header">New Contact Form Submission</div>
-        <div class="content">
-            <p><strong>Name:</strong> ${body.name}</p>
-            <p><strong>Email:</strong> ${body.email}</p>
-            <p><strong>Message:</strong></p>
-            <p>${body.message}</p>
-        </div>
-        <div class="footer">
-            This email was sent from your website's contact form.
-        </div>
-    </div>
-</body>
-</html>`,
+        <!DOCTYPE html>
+        <html lang="en">
+        <!-- ... (rest of the HTML template) ... -->
+        </html>`,
     })
 
     return 'Saadetud'
@@ -82,15 +36,7 @@ export default defineEventHandler(async (event, response) => {
   }
 })
 
-/*   return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Email sent successfully' }),
-    };
-    } catch (error) {
-    console.error(error);
-    return {
-      statusCode: 500, // Or any other appropriate error code
-      body: JSON.stringify({ message: 'Error sending email' }),
-    };
-  }
-}); */
+// Configure the contact endpoint path
+export const config = {
+  path: '/contact',
+}
