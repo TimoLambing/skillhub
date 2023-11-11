a3c4a26a6df54b0a29217869e80db352fe0110a3
Timo <timo@skillhub.ee>
2023-11-11 23:52:02 +0200

    html form

--- .app/netlify/functions/sendMail.js
+++ .app/netlify/functions/sendMail.js
@@ -13,10 +13,61 @@ exports.handler = async (event) => {
   })
 
   const mailOptions = {
-    from: 'timo@skillhub.ee',
-    to: email,
-    subject: subject,
-    text: message,
+    from: process.env.MAILFROM,
+    to: process.env.MAILFROM,
+    subject: body.subject,
+    text: body.message,
+    html: `
+              <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Form Submission</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+        }
+        .email-container {
+            margin: auto;
+            padding: 20px;
+            width: 80%;
+            background-color: #fff;
+            border-radius: 8px;
+        }
+        .header {
+            font-size: 24px;
+            margin-bottom: 20px;
+            color: #333;
+        }
+        .content {
+            margin-bottom: 20px;
+            color: #555;
+        }
+        .footer {
+            font-size: 12px;
+            color: #888;
+        }
+    </style>
+</head>
+<body>
+    <div class="email-container">
+        <div class="header">New Contact Form Submission</div>
+        <div class="content">
+            <p><strong>Name:</strong> ${body.name}</p>
+            <p><strong>Email:</strong> ${body.email}</p>
+            <p><strong>Message:</strong></p>
+            <p>${body.message}</p>
+        </div>
+        <div class="footer">
+            This email was sent from your website's contact form.
+        </div>
+    </div>
+</body>
+</html>`,
   }
 
   try {
